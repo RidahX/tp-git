a553db7de1628733d535083733829ba38512cb9c
RidahX <130641135+RidahX@users.noreply.github.com>
2024-04-12 16:26:08 +0200

    Update calculatrice.py

--- calculatrice.py
+++ calculatrice.py
@@ -3,11 +3,3 @@
 class Calculatrice:
     def addition(self, a, b):
         return a + b
-
-    def soustraction(self, a, b):
-        return a - b
-
-    def division(self, a, b):
-        if b == 0:
-            raise ValueError("La division par zéro n'est pas autorisée.")
-        return a / b
